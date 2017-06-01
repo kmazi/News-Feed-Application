67fb75b1f8fdff31539051c08acdd22cb53147d6
kmazi <kingsleyu13@yahoo.com>
2017-06-01 11:39:11 +0100

    Tested all components

--- __tests__/components/articles.test.js
+++ __tests__/components/articles.test.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import ReactRenderer from 'react-test-renderer';
+import LocalMock from '../../__mocks__/localStorageMock';
+import * as Action from '../../app//actions/actions';
 import Article from './../../app/components/articles.jsx';
 
-describe('The headlines component', () => {
+describe('The articles component', () => {
   it('must render without throwing error', () => {
     const div = document.createElement('div');
-    ReactDom.render(<Article/>, div);
+    ReactDom.render(<Article />, div);
   });
-});
 
-describe('The snapshot test', () => {
-  const component = ReactRenderer.create(<Article/>);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+  it('should render with the exact content', () => {
+    const component = ReactRenderer.create(<Article />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  const eventMock = {
+    target: {
+      getAttribute: (attrib) => {
+        return `${attrib} mock`;
+      }
+    }
+  };
+  eventMock.preventDefault = () => {
+    // just a mock!
+  };
+  const spy = jest.spyOn(LocalMock, 'setItem');
+  describe('before adding favourite articles', () => {
+    it('should check if the user is authenticated', () => {
+      const article = new Article();
+      article.state.isAuthenticated = false;
+      article.addFavourite(eventMock);
+      expect(spy).not.toBeCalled();
+    });
+  });
+
+  it('should allow user to add favourite article when they are logged in',
+  () => {
+    const article = new Article();
+    article.state.isAuthenticated = true;
+    article.addFavourite(eventMock);
+    expect(spy).toBeCalled();
+  });
 });
--- __tests__/components/sources.test.js
+++ __tests__/components/sources.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { shallow } from 'enzyme';
-import MockSourceObj from '../../__mocks__/mockObjects';
+import ReactRenderer from 'react-test-renderer';
 import Sources from './../../app/components/sources.jsx';
 
 describe('The sources component', () => {
@@ -15,9 +15,9 @@ describe('The sources component', () => {
     expect(anchor.text().includes('loading...')).toBe(true);
   });
 
-  it('should load from the main source when no source is being searched',
-  () => {
-    const anchor = shallow(<Sources />);
-    anchor.setState({ articles: MockSourceObj.default });
+  it('should render all component correctly', () => {
+    const component = ReactRenderer.create(<Sources/>);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
